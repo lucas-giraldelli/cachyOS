5d087755d6df957c83674ec96f6fe8531f16bd38
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2026-04-21 16:35:10 -0400

    mako: add notification daemon, fix mangohud frame_timing syntax
    - Add mako config with Tokyo Night theme, bottom-right anchor
    - ignore-timeout, no icons, Chromium/WhatsApp rules
    - Rename "Amor nene" contact display to "Thyci ♥" with red styling
    - Add mako to hyprland autostart, remove dunst
    - Fix MANGOHUD_CONFIG frame_timing=1 → frame_timing (correct syntax)

---
+++ fish/.config/fish/functions/tnsbook.fish
@@ -0,0 +1,3 @@
+function tnsbook --wraps='cd ~/projects/TrackFive/marketplace && npm run storybook --workspace=tools/storybook' --description 'alias tnsbook=cd ~/projects/TrackFive/marketplace && npm run storybook --workspace=tools/storybook'
+    cd ~/projects/TrackFive/marketplace && npm run storybook --workspace=tools/storybook $argv
+end
