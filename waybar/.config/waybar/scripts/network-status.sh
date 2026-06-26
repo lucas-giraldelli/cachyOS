b6f67a9a607595e7e857c6a4f00d78405f35d206
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2026-06-26 09:13:19 -0400

    perf(waybar): use single ping for network status check

--- waybar/.config/waybar/scripts/network-status.sh
+++ waybar/.config/waybar/scripts/network-status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ping -c 3 -W 2 -q google.com 2>/dev/null | grep -q "0% packet loss"; then
+if ping -c 1 -W 2 -q google.com &>/dev/null; then
     echo '{"text": "󰛳", "class": "ok"}'
 else
     echo '{"text": "󰪎", "class": "dead"}'
